13eeeae6a42ed4d839e6a03a841bcdaa8cce1405
vikramp3000 <hellovikram1@gmail.com>
2025-10-20 17:37:03 -0600

    feat: Minor changes UX

--- frontend/app/games/[date]/[team]/page.tsx
+++ frontend/app/games/[date]/[team]/page.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { getGameDetail } from "@/lib/api";
 import { GameDetailResponse } from "@/lib/types";
 import { cleanTeamName } from "@/lib/utils";
-// import EventsTable from "@/app/components/EventsTable";
 import GameFlowRink from "@/app/components/GameFlowRink";
 import RinkChart from "@/app/components/RinkChart";
 
@@ -311,14 +310,6 @@ export default function GameDetailPage() {
           />
         </div>
       </div>
-
-      {/* Events Table */}
-      {/* <div className="mb-8">
-        <h2 className="text-2xl font-bold mb-4">
-          Play-by-Play Events ({events.length})
-        </h2>
-        <EventsTable events={events} />
-      </div> */}
     </main>
   );
 }
